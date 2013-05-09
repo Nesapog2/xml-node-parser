8c16c720f6179574760c4b09f48ef1cc3b0e9f01
robertmryan <robert.ryan@mindspring.com>
2013-05-09 12:38:29 -0400

    Initial Commit

---
+++ NodeParser/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  NodeParser
+//
+//  Created by Robert Ryan on 5/9/13.
+//  Copyright (c) 2013 Robert Ryan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ NodeParser/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  NodeParser
+//
+//  Created by Robert Ryan on 5/9/13.
+//  Copyright (c) 2013 Robert Ryan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
